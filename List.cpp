695f359f539069f08a23ac43da9e35d319451b7f
dominow337 <dominow337@users.noreply.github.com>
2020-03-09 16:37:59 +0100

    nowe

--- List.cpp
+++ List.cpp
@@ -1,22 +1,178 @@
 #include "List.h"
-Node::Node()
-{
-	next = 0;
-}
-List::List()
-{
 
+List::Node * List::find(const size_t idx)
+{
+	Node * node = first;
+
+	for (size_t i = 0; i < idx; i++)
+	{
+		node = node->next; 
+	}
+
+	return node;
 }
+void List::push(const Point & point, const size_t idx)
+{
+	if (idx == 0)
+	{
+		Node * tail = first;
+		first = new Node(point);
+		first->next = tail;
+	}
+	else
+	{
+		Node * prev = find(idx - 1);
+		Node * tail = prev->next;
+		prev->next = new Node(point);
+		prev->next->next = tail;
+	}
+	size++;
+}
+
+void List::push(const List & list, const size_t idx)
+{
+	List copy(list);
+	Node * it = copy.first;
+	if (idx == 0)
+	{
+		Node * tail = first;
+		Node * last = nullptr;
+		if (it)
+		{
+			first = new Node(it->point);
+			last = first;
+			it = it->next;
+		}
+		while (it)
+		{
+			last->next = new Node(it->point);
+			last = last->next;
+			it = it->next;
+		}
+		last->next = tail;
+	}
+	else
+	{
+		Node * prev = find(idx - 1);
+		Node * tail = prev->next;
+		while (it)
+		{
+			prev->next = new Node(it->point);
+			prev = prev->next;
+			it = it->next;
+		}
+		prev->next = tail;
+	}
+	size += list.size;
+}
+
+void List::pop(const size_t idx)
+{
+	if (idx == 0)
+	{
+		Node * tail = first->next;
+		delete first;
+		first = tail;
+	}
+	else
+	{
+		Node * prev = find(idx - 1);
+		Node * tail = prev->next->next;
+		delete prev->next;
+		prev->next = tail;
+	}
+	size--;
+}
+
+List::List() : size(0), first(nullptr){}
+
+List::List(const List & list) : size(list.size), first(nullptr)
+{
+	Node * it = list.first;
+	Node * last = first;
+	if (it)
+	{
+		first = new Node(it->point);
+		last = first;
+		it = it->next;
+	}
+	while (it)
+	{
+		last->next = new Node(it->point);
+		last = last->next;
+		it = it->next;
+	}
+}
+//destruktor
 List::~List()
 {
+	//while (size > 0)
+	//{
+	//	pop_front();
+	//}
 
 }
-List::push_back
-{
-	Node * n = new Node;
-n->word=word;
-if (first == 0)
-{
 
+List::Error List::pop_front()
+{
+	List::Error error = List::Error::SUCCESS;
+
+	if (size == 0)
+	{
+		error = List::Error::LIST_EMPTY;
+	}
+	else
+	{
+		pop(0);
+	}
+
+	return error;
 }
+
+List::Error List::push_back(const Point& point)
+{
+	List::Error error = List::Error::SUCCESS;
+
+		//Node* el = first;
+		//while (el->next)
+		//{
+		//	el = el->next;
+		//}
+		//el->next = new Node(point);
+		//size++;
+	push(point, size);
+	return error;
+}
+
+size_t List::getSize()
+{
+	return size;
+}
+
+List List::operator+(const List& list)
+{
+	List copy(*this);
+	copy.push(list, copy.size);
+	return copy;
+}
+
+List& List::operator+=(const List& list)
+{
+	push(list, size);
+	return *this;
+}
+
+List & List::operator=(const List & list)
+{
+	List copy(list);
+	this->~List(); 
+	push(copy, 0);
+	return *this;
+}
+
+List List::operator--(int)
+{
+	List copy(*this);
+	pop_back();
+	return copy;
 }
--- List.h
+++ List.h
@@ -1,32 +1,43 @@
 #pragma once
-#include <iostream>
-#include <string>
-using namespace std;
+#include <vector>
+#include "Point.h"
 class List
 {
 private:
-	struct Node()
+	struct Node
 	{
-		string word;
+		Point point;
 		Node* next;
-		Node();
+		Node(const Point& p) : point(p.x, p.y, p.z), next(nullptr) {}
 	};
+	size_t size;
+	Node* first;
+
 	Node* find(const size_t);
-	void pop(const size_t);
 	void push(const Point&, const size_t);
+	void push(const List&, const size_t);
+	void pop(const size_t);
+
 public:
+	List();
+	List(const List&);
+	~List();
+	//destruktor w domu!
+
 	enum Error
 	{
-		Success,
-		Index_Out_Of_Bounds,
-		List_Empty
+		SUCCESS,
+		LIST_EMPTY,
+		OUT_OF_BOUNDS //warunki
 	};
-	List();
-	~List();
-	Node* first;
-	pop_back();
-	push_back(const Point&);
-	find(const Point&, const size_t);
-	insert(const Point&, const size_t);
+	Error pop_front();
+	Error push_back(const Point&);
+	size_t getSize();
+	List operator+(const List&);
+	List& operator+=(const List&);
+	List& operator=(const List&);
+	List operator--(int);
+
+	//Proszê pamiêtaæ, ¿e to nie jest pe³na implementacja, nale¿y kod uzupe³niæ zgodnie z instrukcj¹
 };
 
